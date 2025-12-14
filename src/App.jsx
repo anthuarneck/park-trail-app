ee8ecb72429500c04c1b3cd3042949f2803ed680
anthony <131826343+anthuarneck@users.noreply.github.com>
2025-12-14 09:58:57 -0500

    Installs and renders initial leaflet map
    Leaflet map initialized. Tile layer added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 function App() {
+  const mapRef = useRef(null);
+  const mapInstanceRef = useRef(null);
+
+  useEffect(() => {
+    if (mapInstanceRef.current) return;
+
+    mapInstanceRef.current = L.map(mapRef.current).setView([40.8656557, -73.8079112], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap'
+    }).addTo(mapInstanceRef.current);
+
+    return () => {
+      if (mapInstanceRef.current) {
+        mapInstanceRef.current.remove();
+        mapInstanceRef.current = null;
+      }
+    };
+  }, []);
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Park Trails</h1>
+  <div ref={mapRef} id="map" style={{ height: '600px', width: '100%' }}></div>
     </>
   )
 }
