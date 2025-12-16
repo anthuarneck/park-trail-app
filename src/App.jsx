f30284c201e0ed7f584dd0858253fca0a981dcff
anthony <131826343+anthuarneck@users.noreply.github.com>
2025-12-16 17:57:24 -0500

    Adds nyc trail api
    NYC trail api brought in,layer.bindPopup feature implemented.  trails & their properties now popup
     when trails are clicked on

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,42 @@ function App() {
   const mapRef = useRef(null);
   const mapInstanceRef = useRef(null);
 
+  const apiURL = import.meta.env.VITE_API_URL;
+
   useEffect(() => {
     if (mapInstanceRef.current) return;
 
     mapInstanceRef.current = L.map(mapRef.current).setView([40.8656557, -73.8079112], 13);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '© OpenStreetMap'
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png', {
+      maxZoom: 20,
+      attribution: '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapInstanceRef.current);
+
+    fetch(apiURL)
+      .then((response) => response.json())
+      .then((responseJSON) => {
+        L.geoJSON(responseJSON, {
+          onEachFeature: (feature, layer) => {
+            const parkName = feature.properties.park_name
+            const trailName = feature.properties.trail_name
+            const trailDifficulty = feature.properties.difficulty
+            const trailSurface = feature.properties.surface
+            const trailClass = feature.properties.class
+
+            layer.bindPopup(`
+              <div>
+                <h2>Trail Info</h2>
+                <p><strong>Park Name:</strong> ${parkName}</p>
+                <p><strong>Trail Name:</strong> ${trailName}</p>
+                <p><strong>Trail Difficulty:</strong> ${trailDifficulty}</p>
+                <p><strong>Trail Surface:</strong> ${trailSurface}</p>
+                <p><strong>Trail Class:</strong> ${trailClass}</p>
+                </div>`)
+          }
+        }).addTo(mapInstanceRef.current)
+      })
+      .catch((error) => console.log(error))
 
     return () => {
       if (mapInstanceRef.current) {
@@ -28,7 +55,7 @@ function App() {
   return (
     <>
       <h1>Park Trails</h1>
-  <div ref={mapRef} id="map" style={{ height: '600px', width: '100%' }}></div>
+      <div ref={mapRef} id="map" style={{ height: '600px', width: '100%' }}></div>
     </>
   )
 }
