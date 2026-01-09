4b72dd3cd2e11a906dccb226a611dd9a3bb7afd0
anthony <131826343+anthuarneck@users.noreply.github.com>
2026-01-08 19:49:50 -0500

    Adds Pages folder, reorganizes file structure, creates landing page
    Pages folder was added, contents from App.jsx were put into MainPage.jsx. App.jsx
    now handles routes for different pages. Landing page created. TailwindCSS installed
    and utilized on landing page.

---
+++ Pages/MainPage.jsx
@@ -0,0 +1,13 @@
+import Map from "../src/Components/Map.jsx";
+import Weather from "../src/Components/Weather.jsx"
+
+function MainPage() {
+  return (
+    <>
+      <Map />
+      <Weather />
+    </>
+  );
+}
+
+export default MainPage;
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import Map from "./Components/Map.jsx";
-import Weather from "./Components/Weather.jsx"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "../Pages/MainPage";
+import Landing from "../Pages/Landing";
 
 function App() {
   return (
     <>
-      <Map />
-      <Weather />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/map" element={<MainPage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
