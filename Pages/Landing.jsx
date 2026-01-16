3c6f0af16932cad7bf315f20bfb4c876d2fae929
anthony <131826343+anthuarneck@users.noreply.github.com>
2026-01-15 20:33:15 -0500

    updates landing page title and description

--- Pages/Landing.jsx
+++ Pages/Landing.jsx
@@ -22,10 +22,10 @@ function Landing() {
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
             <h1 className="text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">
-            Your next adventure through NYC starts here
+            Get Outside in NYC
             </h1>
             <p className="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
-              Your guide to NYC park trails. View trail difficulty, surface types, current weather and more - all on an interactive map
+              Your guide to NYC park trails. View trail difficulty, surface types, current weather and more - all on an interactive map. Just tap a trail to explore.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
